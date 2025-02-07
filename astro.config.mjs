a650004dcc4b1948fb05e33f21ff5f8e40a037e0
Tobias Onandia <tobiasonandia0@gmail.com>
2025-02-07 12:13:41 -0300

    refactor: add alias for src directory in Vite configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+// @ts-ignore
+import { fileURLToPath } from 'node:url';
 
 // https://astro.build/config
 export default defineConfig({
-
+  vite: {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  },
 });
